c0e73df7f09752c1859d3ee760bb21e68941ed7f
Juan Esteban <110645480+Gunurrea@users.noreply.github.com>
2022-08-21 23:49:58 -0500

    Tercer punto

---
+++ sala de juegos/Program.cs
@@ -0,0 +1,43 @@
+﻿string nombre;
+byte edad;
+decimal descuento1 = 0;
+decimal descuento2= 0;
+var entrada = 15000;
+
+//calculo descuento
+
+descuento1 = Math.Round(Convert.ToDecimal(entrada * 0.05M), 2);
+descuento2 = Math.Round(Convert.ToDecimal(entrada * 0.03M), 2);
+
+//pedir datos
+Console.WriteLine("Ingrese su nombre completo: ");
+nombre=Console.ReadLine();
+
+Console.WriteLine("Ingrese su edad: ");
+edad=Convert.ToByte(Console.ReadLine());
+
+//mostrar resultado
+
+
+if (edad >0 && edad <= 4){
+Console.WriteLine("Su nombre es: " + nombre);
+Console.WriteLine("su edad es: " + edad);
+Console.WriteLine("tiene entrada gratis" );
+}
+
+if (edad >4 && edad <= 18){
+
+Console.WriteLine("Su nombre es: " + nombre);
+Console.WriteLine("su edad es: " + edad);
+Console.WriteLine("tiene un descuento en la entrada de: "+ descuento1 );
+var total = entrada-descuento1;
+Console.WriteLine("TOTAL A PAGAR:" + total);
+}
+
+if(edad > 18 ){
+Console.WriteLine("Su nombre es: " + nombre);
+Console.WriteLine("su edad es: " + edad);
+Console.WriteLine("tiene un descuento en la entrada de: "+ descuento2 );
+var total = entrada-descuento2;
+Console.WriteLine("TOTAL A PAGAR:" + total);
+}
